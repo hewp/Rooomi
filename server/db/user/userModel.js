bbcac28d4be557a136d37088ab03f67cea0db47a
Steven Lin <steventlin.9@gmail.com>
2016-07-30 13:41:57 -0700

    fb oauth authentication

--- server/db/user/userModel.js
+++ server/db/user/userModel.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   fbID: String,
   username: String,
-  password: String,
-  house: String,
+  password: { type: String, default: null },
+  avatar: { type: String, default: 'http://pix.iemoji.com/images/emoji/apple/ios-9/256/pile-of-poo.png' },
+  house: { type: String, default: 'Hack Reactor' },
 });
 
 module.exports = mongoose.model('User', userSchema);
--- server/serverHelpers/authentication.js
+++ server/serverHelpers/authentication.js
@@ -8,7 +8,7 @@ passport.use(new Strategy(
     User.findOne({ username }, (err, user) => {
       if (err) { return cb(err); }
       if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
+      if (user.password !== password) { return cb(null, false); }
       return cb(null, user);
     });
   })
@@ -16,15 +16,32 @@ passport.use(new Strategy(
 
 passport.use(new FBStrategy({
   clientID: '169564226797360',
-    clientSecret: 'f9a6e6f6a452fb18fa720c89d5ced750',
-    callbackURL: '/login/facebook/return',
-    profileFields: ['id', 'name','picture.type(large)', 'emails', 'displayName', 'about', 'gender']
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-    return cb(null, profile);
-  })
-);
+  clientSecret: 'f9a6e6f6a452fb18fa720c89d5ced750',
+  callbackURL: '/login/facebook/return',
+  profileFields: ['id', 'first_name', 'photos', 'emails', 'displayName', 'about', 'gender'],
+},
+  (accessToken, refreshToken, profile, cb) => {
+    console.log(profile.displayName);
+    User.findOne({ fbID: profile.id }).exec()
+    .then((user) => {
+      if (!user) {
+        User.create(
+          { fbID: profile.id,
+            username: profile.displayName.split(' ')[0],
+            avatar: profile.photos[0].value,
+          })
+        .then((data) => {
+          console.log(data);
+          cb(null, data) 
+        });
+        
+      }
+      else {
+        cb(null, user);
+      }
+    });
+  }
+));
 
 passport.serializeUser(function(user, cb) {
   // cb(null, user._id);
